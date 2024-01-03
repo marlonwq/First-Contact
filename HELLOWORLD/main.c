78bed81de85a6b78e2deae7cc887b6d0b307b88d
Márlon Cardoso <sssasuke40@gmail.com>
2024-01-03 02:24:33 -0300

    Addition of new files with the first contact with C# and updating the C and C++ code.

--- HELLOWORLD/main.c
+++ HELLOWORLD/main.c
@@ -1,10 +1,9 @@
 #include <locale.h>
-
-#include <stdio.h> // Pra corrigir e ser exibido caracteres especiais como o acento no "Ol· mundo".
+#include <stdio.h> // Para corrigir e ser exibido caracteres especiais como o acento no "Ol√° mundo".
 
 int main(void)
 {
-    setlocale(LC_ALL, "Portuguese"); //Pra corrigir e ser exibido caracteres especiais como o acento no "Ol· mundo".
-    printf("Ol· Mundo");
+    setlocale(LC_ALL, "Portuguese"); // Para corrigir e ser exibido caracteres especiais como o acento no "Ol√° mundo".
+    printf("Ol√° Mundo");
     return 0;
 }
--- HELLOWORLD/main.cpp
+++ HELLOWORLD/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <locale.h>
+#include <locale.h> // Para corrigir e ser exibido caracteres especiais como o acento no "Olá mundo".
 
 using namespace std;
 
-int main(int arge, char const *argv[])
+int main(int /* arge */, char const *argv[])
 {
+  setlocale(LC_ALL, "Portuguese"); // Para corrigir e ser exibido caracteres especiais como o acento no "Olá mundo".
   std::cout << "Olá Mundo" << std::endl;
   return 0;
 }
